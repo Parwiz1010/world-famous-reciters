755e9475a8f039261ddedf5e6fa2605d508eed17
Parwiz Rahmani <parwiz@gmail.com>
2024-11-13 05:56:30 -0800

    add fontSize

--- lib/container.dart
+++ lib/container.dart
@@ -56,7 +56,7 @@ class _splashState extends State<splash> {
               'خوش آمدید',
               style: TextStyle(fontSize: 30, color: Colors.white),
             ),
-            Text('V 0.2.3',style: TextStyle(),)
+            Text('V 0.2.3',style: TextStyle(fontSize: 5),)
           ],
         ),
       ]),
